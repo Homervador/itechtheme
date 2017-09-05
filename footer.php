20659c39396e8c72109015190e6c7a0bb1c46262
Homervador <pierre.puss@gmail.com>
2017-09-05 12:13:47 +0200

    modification du footer

--- footer.php
+++ footer.php
@@ -1,10 +1,23 @@
 </div> <!-- /.container -->
-
-		<footer class="blog-footer">
-      <p>Blog template built for <a href="http://getbootstrap.com">Bootstrap</a> by <a href="https://twitter.com/mdo">@mdo</a>.</p>
-      <p>
-        <a href="#">Back to top</a>
-      </p>
+    <footer class="blog-footer">
+      <p class="declaration">Déclaration d'activité enregistrée sous le numéro 52 44 07188 44 auprès du préfet de Région de Pays de la Loire>> code APE : 8559A N° RCS 792 116 105</p>
+      <div class="sous-footer">
+        <div class="legal-mention">
+          <p>Mention légales</p>
+          <ul>
+            <li><span class="liste">Parc Héliopolis 1 101 avenue Jacques Cartier 44 800 Saint Herblain</span></li>
+            <li><span class="liste">Mail : contact@ItechSup.fr</span></li>
+            <li><span class="liste">Fax : 02 51 89 03 49</span></li>
+            <li><span class="liste">Tel : 02 51 89 01 69</span></li>
+          </ul>
+        </div>
+        <div class="link">
+          <p>
+            Connexion
+          </p>
+          
+        </div>
+      </div>
     </footer>
 
 	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
